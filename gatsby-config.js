5c14d7c44bde7ec527f61f80431062a5102abcc5
Thada Wangthammang <mildronize@gmail.com>
2019-11-10 01:05:44 +0700

    init project

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `An example to learn how to source data form WordPress`,
+    subtitle: `Sourcing data from WordPress`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `host.mildronize.com`,
+        protocol: `https`,
+        hostingWPCOM: false,
+        useACF: false,
+        excludedRoutes: ["/wp/v2/users/me", "/acf/v2/options", "/wp/v2/settings"]
+      },
+    },
+  ],
+}
