6f2097211a8c0d6b5af65aa7376b8443e10bb118
Thada Wangthammang <mildronize@gmail.com>
2019-11-12 01:50:38 +0700

    add lazysized for lazy load image

--- src/components/layouts/PageLayout.js
+++ src/components/layouts/PageLayout.js
@@ -3,6 +3,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
+// https://web.dev/codelab-use-lazysizes-to-lazyload-images/
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+
 class PageLayout extends Component {
   render() {
     const { children } = this.props;
