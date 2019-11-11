dacd445d98aaff44a05da2dd31f9b678ccbc4261
Thada Wangthammang <mildronize@gmail.com>
2019-11-11 21:28:00 +0700

    remove state of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,48 +2,43 @@ import React, { Component } from "react"
 import { Link, graphql } from "gatsby"
 import PageLayout from "../components/layouts/PageLayout"
 import PostList from "../components/PostList"
-import QueryString from 'query-string';
-import PostContent from "../components/PostContent";
+// import QueryString from 'query-string';
+// import PostContent from "../components/PostContent";
 
 class Homepage extends Component {
 
-  state = {
-    isPreview: false,
-    pid: null,
-  }
+  // state = {
+  //   isPreview: false,
+  //   pid: null,
+  // }
 
-  componentDidMount(){
-      this.checkParam();
-  }
+  // componentDidMount(){
+  //     this.checkParam();
+  // }
 
-  checkParam(){
-    const parsed = QueryString.parse(window.location.search);
-    const { p } = parsed;
-    if(!(p === undefined || p === "" || p === null)){
-      this.setState({ isPreview: true, pid: p });
-    }
-  }
+  // checkParam(){
+  //   const parsed = QueryString.parse(window.location.search);
+  //   const { p } = parsed;
+  //   if(!(p === undefined || p === "" || p === null)){
+  //     this.setState({ isPreview: true, pid: p });
+  //   }
+  // }
 
   render() {
     const data = this.props.data
 
     return (
       <PageLayout>
-        {this.state.isPreview?
-        <PostContent p={this.state.pid} />:
-          <>
-            <span
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: data.wordpressPage.content,
-                }}
-              />
-            <hr />
-            <div className="page-section-header">Latest Posts</div>
-            <PostList posts={data.allWordpressPost.edges} />
-            <center><Link to="/blog/">All blog posts</Link></center>
-            </>
-          }
+          <span
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: data.wordpressPage.content,
+              }}
+            />
+          <hr />
+          <div className="page-section-header">Latest Posts</div>
+          <PostList posts={data.allWordpressPost.edges} />
+          <center><Link to="/blog/">All blog posts</Link></center>
       </PageLayout>
     )
   }
