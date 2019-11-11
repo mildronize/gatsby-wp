02d4f5aeef3f8998449505ace65e9eba8454b860
Thada Wangthammang <mildronize@gmail.com>
2019-11-11 19:35:08 +0700

    add admin page fix styles

---
+++ src/pages/admin.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class BlogPage extends Component {
+
+    constructor() {
+        window.location.replace("https://host.mildronize.com/wp-admin");
+    }
+
+    render() {
+        return <></>;
+    }
+}
+
+export default BlogPage
